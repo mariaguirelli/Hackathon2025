66c7ca80128ac5b10b1feabbeb9048c1ad601c0e
mariaguirelli <eduardaguirelli89@gmail.com>
2025-06-19 21:23:42 -0300

    atualização crud professor

--- java/src/main/java/edu/unialfa/java/repository/ProfessorRepository.java
+++ java/src/main/java/edu/unialfa/java/repository/ProfessorRepository.java
@@ -1,7 +1,15 @@
 package edu.unialfa.java.repository;
 
+import edu.unialfa.java.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import edu.unialfa.java.model.Professor;
 
+import java.util.Optional;
+
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+    Optional<Professor> findByRegistro(String registro);
+
+    boolean existsByRegistro(String registro);
+
+    Professor findByCpf(String cpf);
 }
